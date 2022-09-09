c2759f8a37e81033a11dac10b974353a4b6e50e0
PorridgePower <>
2022-09-09 11:08:01 -0400

    Add tests for log opener

---
+++ tests/test_latest_log.py
@@ -0,0 +1,21 @@
+import unittest
+from os import path
+from time import strptime
+from log_analyzer import get_latest_log, LogInfo
+
+
+class TestLatestLogSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.testdir = path.join(path.dirname(path.abspath(__file__)),
+                                 "testdir")
+
+    def test_valid_name(self):
+        self.assertEqual(
+            get_latest_log(self.testdir),
+            LogInfo(path.join(self.testdir, 'nginx-access-ui.log-20220909.gz'),
+                    'gz', strptime('20220909', "%Y%m%d")))
+
+
+if __name__ == '__main__':
+    unittest.main()
