27fae2b286520116d383f42149c3198f6f557bd9
PorridgePower <>
2023-01-19 21:07:58 +0300

    Fix tests formatting

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,23 +4,41 @@ from log_analyzer import init_configuration, CONFIG
 
 
 class TestConfiguration(unittest.TestCase):
-
     def setUp(self):
-        self.testdir = path.join(path.dirname(path.abspath(__file__)),
-                                 "testdir")
+        self.testdir = path.join(path.dirname(path.abspath(__file__)), "testdir")
 
     def test_not_existing_file(self):
-        self.assertEqual(init_configuration(
-            default_conf=CONFIG, specified_conf=path.join(self.testdir, "not_existing.conf")), CONFIG)
+        self.assertEqual(
+            init_configuration(
+                default_conf=CONFIG,
+                specified_conf=path.join(self.testdir, "not_existing.conf"),
+            ),
+            CONFIG,
+        )
 
     def test_invalid_config(self):
-        self.assertEqual(init_configuration(
-            default_conf=CONFIG, specified_conf=path.join(self.testdir, "invalid.conf")), CONFIG)
+        self.assertEqual(
+            init_configuration(
+                default_conf=CONFIG,
+                specified_conf=path.join(self.testdir, "invalid.conf"),
+            ),
+            CONFIG,
+        )
 
     def test_merge(self):
-        self.assertEqual(init_configuration(
-            default_conf=CONFIG, specified_conf=path.join(self.testdir, "valid.conf")), {'REPORT_SIZE': "100", 'REPORT_DIR': './test_reports', 'LOG_DIR': './log'})
+        self.assertEqual(
+            init_configuration(
+                default_conf=CONFIG,
+                specified_conf=path.join(self.testdir, "valid.conf"),
+            ),
+            {
+                "REPORT_SIZE": "100",
+                "REPORT_DIR": "./test_reports",
+                "OUTPUT": None,
+                "LOG_DIR": "./log",
+            },
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
